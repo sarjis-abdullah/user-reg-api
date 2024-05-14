332a4c42841b28ca6a8b486169c4b7412a562eb8
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-05-14 10:46:38 +0600

    mail default settings updated

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'sandbox.smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAIL_USERNAME', 'bbff34bebebdf2'),
+            'password' => env('MAIL_PASSWORD', 'd4274f57c49383'),
             'timeout' => null,
             'auth_mode' => null,
         ],
